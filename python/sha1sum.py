cc2dc7d804191b70994da6c94b55948f21c5b7ff
Thurston Stone <tstone2077@gmail.com>
2011-10-27 15:49:55 -0700

    Adding a progress bar for generating sha1 hashes on files/directories.

--- python/sha1sum.py
+++ python/sha1sum.py
@@ -17,6 +17,7 @@ import os
 from optparse import OptionParser
 import logging
 import hashlib
+from progressbar import ProgressBar, Counter, Timer, Percentage, Bar
 
 
 INVALID_USAGE_RETURN_CODE = 2
@@ -59,36 +60,90 @@ def validateArgs():
    if error is not None: raise InvalidUsage(parser,error)
    return opts,args
 
-def sha1(paths):
-   """
-   Recursively calculates a checksum representing the contents of all files
-   found with a sequence of file and/or directory paths.
-   """
-   if not hasattr(paths, '__iter__'):
-      raise TypeError('sequence or iterable expected not %r!' % type(paths))
-
-   def update_checksum(checksum, dirname, filenames):
-      for filename in sorted(filenames):
-         path = os.path.join(dirname, filename)
-         if os.path.isfile(path):
-            logging.debug("Hashing %s"%path)
-            fh = open(path, 'rb')
+class HashPath:
+   def __init__(self,path,showProgress=False):
+      if not os.path.exists(path):
+         raise OSError("File Not Found")
+      self.root = path
+      self.checksum = hashlib.sha1()
+      self.path = os.path.abspath(path)
+      #get a directory/file listing for this path as well as the total size
+      (self.listing,self.size) = self.__scan_path(showProgress)
+   def __str__(self):
+      return "Size=%d,ListingLength=%d,Path=%s"%(self.size,len(self.listing),self.path)
+   def __scan_path(self,showProgress):
+      logging.debug("Scanning for files and sizes of %s"%self.path)
+      size = 0
+      listing = []
+      if os.path.isdir(self.path):
+         if showProgress:
+            pbar = ProgressBar(1,[self.path," : ", Counter()," Files scanned. ", Timer()])
+            pbar.start()
+         for root,dirs,files in os.walk(self.path):
+            if len(files) == 0:
+               listing.append(root)
+            else:
+               for file in files:
+                  filename = os.path.join(root,file)
+                  size += os.path.getsize(filename)
+                  listing.append(filename)
+                  length = len(listing)
+                  if showProgress:
+                     pbar.maxval = length
+                     if length%25==0:
+                        pbar.update(length)
+         if showProgress:
+            pbar.finish()
+      else:
+         listing.append(os.path.basename(self.path))
+         size+=os.path.getsize(self.path)
+      listing.sort()
+      return (listing,size)
+   def calculateHash(self,pbar=None):
+      for file in self.listing:
+         if os.path.isfile(file):
+            #we only want to operate on files
+            logging.debug("Hashing file: %s"%file)
+            fh = open(file)
+            if pbar is not None:
+               startPos = pbar.currval
             while 1:
                buf = fh.read(4096)
                if not buf : break
-               checksum.update(buf)
+               self.checksum.update(buf)
+               if pbar is not None:
+                  #raw_input("%s: %d/%d = %d"%(file,startPos+fh.tell(),pbar.maxval,(startPos+fh.tell())/pbar.maxval*100))
+                  pbar.update(startPos+fh.tell())
             fh.close()
+      return self.checksum.hexdigest()
 
-   chksum = hashlib.sha1()
+def sha1(paths,showProgress=False):
+   hashPaths = []
+   hashes = []
+   totalSize = 0
+   for path in paths:
+      hashPath = HashPath(path,showProgress)
+      hashPaths.append(hashPath)
+      totalSize += hashPath.size
 
-   for path in sorted([os.path.normpath(f) for f in paths]):
-      if os.path.exists(path):
-         if os.path.isdir(path):
-            os.path.walk(path, update_checksum, chksum)
-         elif os.path.isfile(path):
-            update_checksum(chksum, dirname(path), basename(path))
+   size = totalSize
+   suffixes=['B','KB','MB','GB','TB']
+   suffixIndex = 0
+   precision = 2
+   while size > 1024:
+      suffixIndex += 1 #increment the suffix
+      size = size/1024.0 #apply the division
 
-   return chksum.hexdigest()
+   logging.info("Hashing %.*f%s"%(precision,size,suffixes[suffixIndex]))
+   pbar=None
+   if showProgress:
+      pbar = ProgressBar(totalSize,[Counter()," ",Percentage(),Bar(),Timer()])
+      pbar.start()
+   for hashPath in hashPaths:
+      hashes.append(hashPath.calculateHash(pbar))
+      logging.info(hashPath.checksum.hexdigest())
+   if showProgress:
+      pbar.finish()
 
 def main():
    """  
@@ -100,7 +155,7 @@ def main():
    logging.basicConfig(level=level,
                     format= '' + LOG_MESSAGE_PREFIX + ':[%(asctime)s]:[%(levelname)s]: %(message)s')
 					
-   return sha1(args)
+   return sha1(args,True)
 
 # if this program is run on the command line, run the main function
 if __name__ == '__main__':
